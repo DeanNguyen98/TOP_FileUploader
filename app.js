533d9eae508bcb8a4a0fea19ad136f20cb0a7462
DeanNguyen98 <nctuan.hn@gmail.com>
2025-02-04 21:46:49 +1100

    implement prisma session store

--- app.js
+++ app.js
@@ -1,10 +1,36 @@
 const path = require("node:path");
 const express = require("express");
-
-const app = express();
+const queries = require("./prisma/queries")
+const authRoutes = require("./router/auth");
+const session = require("express-session")
+const { PrismaSessionStore } = require("@quixo3/prisma-session-store")
+const { PrismaClient, Prisma } = require("@prisma/client");
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 
-app.get("/", (req,res) => res.render("Login"));
+const app = express();
+const prisma = new PrismaClient();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(
+    session({
+        secret: process.env.SESSION_SECRET,
+        resave: false,
+        saveUninitialized: false,
+        cookie: { maxAge: 30 * 24 * 60 * 60 * 1000 },
+        store: new PrismaSessionStore(prisma, {
+            checkPeriod: 2 * 60 * 1000,
+            dbRecordIdIsSessionId: true, 
+            dbRecordIdFunction: undefined,
+        })
+    })
+)
+app.use("/auth", authRoutes);
+
+
+
+
 
 app.listen(3000, () => console.log("listening"));
