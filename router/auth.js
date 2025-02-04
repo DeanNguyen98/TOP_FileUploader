e48cba5005f3326c174b8d5332c3c89bdd54489c
DeanNguyen98 <nctuan.hn@gmail.com>
2025-02-05 08:06:04 +1100

    implement register validation logic

--- router/auth.js
+++ router/auth.js
@@ -2,6 +2,10 @@ const express = require("express");
 const router = express.Router();
 const queries = require("../prisma/queries");
 const passport = require("passport");
+const validateUser = require("../lib/validator");
+const {validationResult} = require("express-validator");
+const { validPassword, genPassword } = require("../lib/authentication");
+
 router.get("/login", (req, res) => {
     res.render("../views/Login");
 })
@@ -20,9 +24,17 @@ router.get("/register", (req, res) => {
     res.render("Register");
 })
 
-router.post("/register", async (req, res) => {
-
-    const user =  await queries.registerUser(req.body.username, req.body.password);
+router.post("/register",validateUser, async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(400).render("Register", {
+            errors: errors.array()
+        })
+    }
+    const saltHash = genPassword(req.body.password);
+    const salt = saltHash.salt;
+    const hash = saltHash.hash
+    const user =  await queries.registerUser(req.body.username, hash, salt);
     console.log("user created", user)
     res.send("user created");
 })
